c22cf38d36009feedce2aba21dbe8fef4867e337
Linh Pete <peterdamlinh1215@gmail.com>
2024-09-19 10:45:56 +0700

    demo packaging & access modifier

---
+++ src/main/java/Package1/A.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Package1;
+
+import Package2.D;
+
+/**
+ *
+ * @author ndhlt
+ */
+class A extends D {
+    B b = new B();
+    public static void main(String[] args) {
+        System.out.println(E.hello);
+//        System.out.println(D.pi);
+    }
+}
---
+++ src/main/java/Package2/D.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Package2;
+
+/**
+ *
+ * @author ndhlt
+ */
+public class D {
+    static protected class E{
+
+        
+        public static String hello = "hello";
+    }
+    
+    private static double pi = 3.14;
+}
